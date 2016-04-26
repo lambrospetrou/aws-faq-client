c3ed2f1b50399ddbab874b5e60c7d7b5f9cebfa8
Lambros Petrou <lambrospetrou@gmail.com>
2016-04-27 00:36:57 +0100

    Some refactoring with the string names.

--- application.go
+++ application.go
@@ -7,14 +7,17 @@ import (
 	"strings"
 
 	"io/ioutil"
-	_ "net/http/pprof"
 	"os"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 const (
-	SERVICES_MAIN string = "iam,kms,waf,directoryservice,ec2,vpc,ebs,elasticbeanstalk,sns,sqs,swf,s3,dynamodb,elasticache,glacier,rds,cloudfront,route53,directconnect,storagegateway,cloudformation,cloudwatch,cloudtrail,config"
+	SERVICES_MAIN string = `iam,kms,waf,directoryservice,
+							ec2,vpc,ebs,elasticbeanstalk,sns,sqs,swf,
+							s3,dynamodb,elasticache,glacier,rds,
+							cloudfront,route53,directconnect,storagegateway,
+							cloudformation,cloudwatch,cloudtrail,config`
 )
 
 func main() {
@@ -31,21 +34,23 @@ func main() {
 		return
 	}
 	fmt.Println("\n## Usage\n")
-	fmt.Println("[-a] Fetch all the main services", SERVICES_MAIN)
+	fmt.Println("[-a] Fetch all the main services. If provided, '-s' will be ignored.", SERVICES_MAIN)
 	fmt.Println("[-s] Comma separated services: i.e. '-s s3,ec2'")
 }
 
 func FetchServices(servicesStr string, outDir string) {
-	fmt.Println("Will fetch: ", servicesStr)
 	services := strings.Split(servicesStr, ",")
+	fmt.Println(":: Will fetch", len(services), "services! ::")
 	for _, service := range services {
 		ParseFAQ(service, outDir)
 	}
+	fmt.Println(":: Finished fetching! ::")
 }
 
 func ParseFAQ(name string, outDir string) error {
+	name = strings.TrimSpace(name)
 	fmt.Println("Fetching", name)
-	doc, err := goquery.NewDocument("https://aws.amazon.com/" + strings.TrimSpace(name) + "/faqs/")
+	doc, err := goquery.NewDocument("https://aws.amazon.com/" + name + "/faqs/")
 	if err != nil {
 		log.Fatal(err)
 	}

2723a4cf6f157cbad72654e6a373363b768dcacb
Lambros Petrou <lambrospetrou@gmail.com>
2016-04-27 00:26:08 +0100

    Added flag for output directory to store FAQs

--- application.go
+++ application.go
@@ -14,19 +14,20 @@ import (
 )
 
 const (
-	SERVICES_MAIN string = "ec2,vpc,ebs,s3,dynamodb,glacier,rds,cloudfront,route53,directconnect,storagegateway,sns,sqs,cloudwatch,cloudtrail,config"
+	SERVICES_MAIN string = "iam,kms,waf,directoryservice,ec2,vpc,ebs,elasticbeanstalk,sns,sqs,swf,s3,dynamodb,elasticache,glacier,rds,cloudfront,route53,directconnect,storagegateway,cloudformation,cloudwatch,cloudtrail,config"
 )
 
 func main() {
-	servicesInput := flag.String("s", "", "Please give the service name you want to read FAQ.")
-	servicesMain := flag.Bool("a", false, "You will create the FAQs for all services")
+	servicesInput := flag.String("s", "", "Service names you want to download FAQs.")
+	servicesMain := flag.Bool("a", false, "You will create the FAQs for all main services.")
+	outDir := flag.String("o", "faqs", "The output dir that will contain all the downloaded FAQs")
 	flag.Parse()
 
 	if *servicesMain {
-		FetchServices(SERVICES_MAIN)
+		FetchServices(SERVICES_MAIN, *outDir)
 		return
 	} else if len(*servicesInput) > 0 {
-		FetchServices(*servicesInput)
+		FetchServices(*servicesInput, *outDir)
 		return
 	}
 	fmt.Println("\n## Usage\n")
@@ -34,15 +35,15 @@ func main() {
 	fmt.Println("[-s] Comma separated services: i.e. '-s s3,ec2'")
 }
 
-func FetchServices(servicesStr string) {
+func FetchServices(servicesStr string, outDir string) {
 	fmt.Println("Will fetch: ", servicesStr)
 	services := strings.Split(servicesStr, ",")
 	for _, service := range services {
-		ParseFAQ(service)
+		ParseFAQ(service, outDir)
 	}
 }
 
-func ParseFAQ(name string) error {
+func ParseFAQ(name string, outDir string) error {
 	fmt.Println("Fetching", name)
 	doc, err := goquery.NewDocument("https://aws.amazon.com/" + strings.TrimSpace(name) + "/faqs/")
 	if err != nil {
@@ -59,11 +60,11 @@ func ParseFAQ(name string) error {
 		log.Fatal(err)
 	}
 
-	if err := os.MkdirAll("faqs", 0755); err != nil {
-		fmt.Println("Could not create 'faqs' directory!")
+	if err := os.MkdirAll(outDir, 0755); err != nil {
+		fmt.Println("Could not create output directory:", err)
 		return err
 	}
 
-	fmt.Println("Writing FAQ html has error:", ioutil.WriteFile("faqs/faq-"+name+".html", []byte(faqHtml), 0644))
+	fmt.Println("Writing FAQ html has error:", ioutil.WriteFile(outDir+"/faq-"+name+".html", []byte(faqHtml), 0644))
 	return nil
 }
